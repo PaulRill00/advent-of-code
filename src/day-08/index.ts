8d5a7b1a51f9517a373684cd5a144b7d74f592d0
Paul Rill <paulrill2000@outlook.com>
2023-12-08 09:21:55 +0100

    feat: :sparkles: day-08 - Part One

---
+++ src/day-08/index.ts
@@ -0,0 +1,37 @@
+import { DayFunction } from '../DayFunction';
+
+const dayFn: DayFunction = (input) => {
+    const [sequence, , ..._maps] = input;
+
+    const maps = new Map<string, { L: string; R: string }>();
+
+    _maps.forEach(map => {
+        const [start, _lr] = map.split(' = ');
+        const lr = _lr.replaceAll('(', '').replaceAll(')', '').split(', ');
+
+        maps.set(start, {
+            L: lr[0],
+            R: lr[1] 
+        })
+    })
+
+    let current = 'AAA';
+    let currentSeqIndex = 0;
+    let stepCount = 0;
+
+    while (current !== 'ZZZ') {
+        const directions = maps.get(current)!;
+        const direction = sequence[currentSeqIndex] as 'L'|'R';
+
+        current = directions[direction];
+
+        currentSeqIndex = (currentSeqIndex + 1) % sequence.length;
+        stepCount++;
+
+    }
+    
+
+    return `Amount of steps: ${stepCount}`;
+}
+
+export default dayFn;
